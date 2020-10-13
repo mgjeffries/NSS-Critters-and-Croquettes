0beb6a0c2109ddfdb13bbdd29ef5f71da29e2c30
mgjeffries <gib.jeffries@gmail.com>
2020-10-13 15:26:55 -0500

    add classes for critters

---
+++ animals.py
@@ -0,0 +1,121 @@
+class Donkey():
+    def __init__(self, name, species, date_added):
+        self.name = name, 
+        self.species = species
+        self.date_added = date_added
+        self.area = 'petting area'
+        self.walking = True
+
+class Llama():
+    def __init__(self, name, species, date_added):
+        self.name = name, 
+        self.species = species
+        self.date_added = date_added
+        self.area = 'petting area'
+        self.walking = True
+
+class Goat():
+    def __init__(self, name, species, date_added):
+        self.name = name, 
+        self.species = species
+        self.date_added = date_added
+        self.area = 'petting area'
+        self.walking = True
+
+class Chicken():
+    def __init__(self, name, species, date_added):
+        self.name = name, 
+        self.species = species
+        self.date_added = date_added
+        self.area = 'petting area'
+        self.walking = True
+
+class Ostrich():
+    def __init__(self, name, species, date_added):
+        self.name = name, 
+        self.species = species
+        self.date_added = date_added
+        self.area = 'petting area'
+        self.walking = True
+
+class Copperhead():
+    def __init__(self, name, species, date_added):
+        self.name = name, 
+        self.species = species
+        self.date_added = date_added
+        self.area = 'glass tank'
+        self.slithering = True
+
+class RatSnake():
+    def __init__(self, name, species, date_added):
+        self.name = name, 
+        self.species = species
+        self.date_added = date_added
+        self.area = 'glass tank'
+        self.slithering = True
+
+class SnappingTurtle():
+    def __init__(self, name, species, date_added):
+        self.name = name, 
+        self.species = species
+        self.date_added = date_added
+        self.area = 'glass tank'
+        self.walking = True
+
+class Mouse():
+    def __init__(self, name, species, date_added):
+        self.name = name, 
+        self.species = species
+        self.date_added = date_added
+        self.area = 'glass tank'
+        self.walking = True
+
+class Armadillo():
+  def __init__(self, name, species, date_added):
+    self.name = name, 
+    self.species = species
+    self.date_added = date_added
+    self.area = 'glass tank'
+    self.walking = True
+
+
+class Mallard():
+  def __init__(self, name, species, date_added):
+    self.name = name, 
+    self.species = species
+    self.date_added = date_added
+    self.area = 'pond'
+    self.swimming = True
+    self.walking = True
+
+class Goldfish():
+  def __init__(self, name, species, date_added):
+    self.name = name, 
+    self.species = species
+    self.date_added = date_added
+    self.area = 'pond'
+    self.swimming = True
+
+class Frog():
+  def __init__(self, name, species, date_added):
+    self.name = name, 
+    self.species = species
+    self.date_added = date_added
+    self.area = 'pond'
+    self.swimming = True
+
+class Trout():
+  def __init__(self, name, species, date_added):
+    self.name = name, 
+    self.species = species
+    self.date_added = date_added
+    self.area = 'pond'
+    self.swimming = True
+
+class Crawfish():
+  def __init__(self, name, species, date_added):
+    self.name = name, 
+    self.species = species
+    self.date_added = date_added
+    self.area = 'pond'
+    self.swimming = True
