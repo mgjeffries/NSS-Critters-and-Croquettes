48599f5a0c8ccdf8a4ec3db2be662d8518bd66b8
mgjeffries <gib.jeffries@gmail.com>
2020-10-19 15:38:58 -0500

    add classes for attractions

---
+++ attractions.py
@@ -0,0 +1,7 @@
+class Attraction:
+  def __init__(self, name, description):
+      self.attraction_name = name
+      self.description = description
+      self.animals = list()
+
+
--- index.py
+++ index.py
@@ -1,5 +1,12 @@
 from animals import *
+from attractions import Attraction
 
 fred = Donkey( 'fred', 'Donkey', 'Morning')
+george = Donkey( 'george', 'Donkey', 'Morning')
+varmint_village = Attraction("Varmint Village", "cute and fuzzy critters to cuddle")
+aviary = Attraction("The Roost", "winged and clawed critters")
+varmint_village.animals.append( fred )
+varmint_village.animals.append( george )
 
-print(fred)
+for animal in varmint_village.animals:
+  print(f"{animal.name} is a {animal.species} that can be found in the place")
